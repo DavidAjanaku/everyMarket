08423a857382d449d4b45e3c1aa872bdd9a95c74
David Ajanaku <davidajanaku46163@gmail.com>
2022-09-25 00:42:41 +0100

    first comit

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -6,6 +6,10 @@ const mobileMenu = document.querySelector('.mobile-bottom-navigations');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.menu-close-button');
 
+const mobileSidebar = document.querySelector('.mobile-sidebar')
+
+const sidebarCategory = document.querySelector('.sidebar-category');
+
 mobileNavIcon.addEventListener('click', function(){
     console.log(mobileMenu);
     mobileMenu.classList.add('active')
@@ -17,15 +21,30 @@ mobileNavIcon.addEventListener('click', function(){
 })
 
 
+sidebarCategory.addEventListener('click', function(){
+    mobileSidebar.classList.add('active')
+    overlay.classList.add('active')
+    sidebarCategory.classList.add('active')
+
+
+})
 
 closeBtn.addEventListener('click', function(){
     mobileMenu.classList.remove('active')
     overlay.classList.remove('active')
+    mobileSidebar.classList.remove('active')
+
     console.log(closeBtn);
 })
 
+
+
 overlay.addEventListener('click', function(){
-    overlay.classList.remove('active');
     mobileMenu.classList.remove('active')
+    mobileSidebar.remove('active');
+    overlay.classList.remove('active');
+   
 
 })
+
+
