36922d15d5206e3bb9c27667644744c49674fc25
David Ajanaku <davidajanaku46163@gmail.com>
2022-09-24 23:06:13 +0100

    first comit

---
+++ js/mobileNav.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const mobileNavIcon = document.querySelector('.mobile-click');
+
+const mobileMenu = document.querySelector('.mobile-bottom-navigations');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.menu-close-button');
+
+mobileNavIcon.addEventListener('click', function(){
+    console.log(mobileMenu);
+    mobileMenu.classList.add('active')
+    overlay.classList.add('active')
+
+
+
+   
+})
+
+closeBtn.addEventListener('click', function(){
+    mobileMenu.classList.remove('active')
+    overlay.classList.remove('active')
+    console.log(closeBtn);
+})
